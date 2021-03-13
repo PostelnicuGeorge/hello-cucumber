a31a277709ebacc8a1b25ef7e3521ea3f093eaf5
George POSTELNICU <george.postelnicu@ext.ec.europa.eu>
2021-03-13 20:14:57 +0100

    Added ReportService.java and reporting package

--- src/main/java/ro/agile_testing/hello_cucumber/RunCucumberTest.java
+++ src/main/java/ro/agile_testing/hello_cucumber/RunCucumberTest.java
@@ -1,15 +1,17 @@
 package ro.agile_testing.hello_cucumber;
 
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-import org.testng.annotations.DataProvider;
+import io.cucumber.core.cli.Main;
+import ro.agile_testing.hello_cucumber.services.ReportService;
 
-@CucumberOptions(plugin = {"pretty", "json:target/jsonReports/test-results.json"})
-public class RunCucumberTest extends AbstractTestNGCucumberTests {
+//@CucumberOptions(plugin = {"pretty", "json:target/jsonReports/test-results.json"})
+public class RunCucumberTest {
 
-	@DataProvider(parallel = true)
-	@Override
-	public Object[][] scenarios() {
-		return super.scenarios();
+	public static void main(String[] args) {
+		String[] argv = new String[]{"--plugin", "pretty", "json:target/jsonReports/test-results.json"};
+		ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
+		byte exitStatus = Main.run(argv, contextClassLoader);
+		System.out.println(exitStatus);
+		ReportService reportService = new ReportService();
+		reportService.generateReport();
 	}
 }
