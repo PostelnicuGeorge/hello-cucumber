f4557df3a9dfcde5c41ee71ef526ccc411980481
George POSTELNICU <george.postelnicu@ext.ec.europa.eu>
2021-03-13 19:33:58 +0100

    Added Hooks.java and changed the maven output format to JUnit

---
+++ src/test/java/ro/agile_testing/hello_cucumber/hooks/Hooks.java
@@ -0,0 +1,21 @@
+package ro.agile_testing.hello_cucumber.hooks;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+
+public class Hooks {
+	@Before
+	public void before(Scenario scenario) {
+		logLine("Before :: ", scenario);
+	}
+
+	@After
+	public void after(Scenario scenario) {
+		logLine("After :: ", scenario);
+	}
+
+	private void logLine(String prefix, Scenario scenario) {
+		System.out.println(prefix + Thread.currentThread().getName() + " :: " + scenario.getName());
+	}
+}
